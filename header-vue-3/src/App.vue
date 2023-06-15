b56cb3c90357a3fc76ccbb99881f12599546cb86
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-15 13:20:44 +0200

    first commit main structure

---
+++ header-vue-3/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import HeaderApp from './components/HeaderApp.vue'
+export default{
+  name:"AppHeader"
+}
+
+</script>
+
+<template>
+  <div>
+    
+  </div>
+  <HeaderApp/>
+</template>
+
+<style>
+@import url(./assets/styles/general.scss);
+</style>
